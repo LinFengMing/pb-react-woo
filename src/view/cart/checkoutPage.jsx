8c40097420241ee734be33f193488aff2e8606a8
jiro <jiro853141@gmail.com>
2021-06-03 16:45:16 +0800

    移植地址編輯器(加入Class Component於Hooks)

--- src/view/cart/checkoutPage.jsx
+++ src/view/cart/checkoutPage.jsx
@@ -1,23 +1,23 @@
-import React, { useState, useContext, useEffect } from 'react'
+import React, { useState, useContext, useEffect, useRef } from 'react'
 import Button from '@material/react-button'
 import CartContext from '../../context/cartContext'
 import OrderService from '../../services/orderService'
 import CartService from '../../services/cartService'
-import CustomerService from '../../services/customerService'
-import IsLogInContext from '../../context/isLogInContext'
+import CheckoutInfoEditorContainer from './components/class/checkoutInfoEditorContainer'
+import ExtraCheckoutInfroContext from '../../context/extraCheckoutInfroContext'
+import CheckoutInfoContext from '../../context/checkoutInfoContext'
 
 const orderService = new OrderService()
 const cartService = new CartService()
-const customerService = new CustomerService()
 
 const data = {
     payment_method: "bacs",
     payment_method_title: "銀行轉帳",
-    set_paid: false,
+    set_paid: true,
     billing: {
         first_name: "小明",
         last_name: "王",
-        address_1: "信義路五段7號89樓",
+        address_1: "",
         address_2: "",
         city: "信義區",
         state: "台北市",
@@ -29,7 +29,7 @@ const data = {
     shipping: {
         first_name: "小明",
         last_name: "王",
-        address_1: "信義路五段7號89樓",
+        address_1: "",
         address_2: "",
         city: "信義區",
         state: "台北市",
@@ -43,18 +43,32 @@ const data = {
             method_title: "Flat Rate",
             total: "10"
         }
-    ],
-    customer_id: customerService.getCustomerIdFromCookie()
+    ]
 };
+
 
 const CheckoutPage = () => {
     const [submitting, setSubmitting] = useState(false)
+    const [submitData, setSubmitData] = useState(data)
+    const [extraSubmitData, SetExtraSubmitData] = useState({
+        receiptType: "2",
+        taxId: "",
+        receiptOptions: ["byMail"]
+    })
     const [cartItemDetails] = useContext(CartContext)
-    const { shipping } = data
-    const buttonText = (submitting) ? "結帳中...請稍後" : "結帳"
-    const [isLogin, setIsLogin] = useContext(IsLogInContext)
+    const isReady = useRef(false)
 
-    data.line_items = cartItemDetails.map((item) => {
+    const getButtonText = () => {
+        if (submitting) {
+            return "結帳中...請稍後"
+        } else if (!isReady.current) {
+            return "資料填寫不完整, 無法結帳"
+        } else {
+            return "結帳"
+        }
+    }
+
+    submitData.line_items = cartItemDetails.map((item) => {
         return {
             product_id: item.product.id,
             quantity: item.quantity
@@ -66,41 +80,36 @@ const CheckoutPage = () => {
         orderService.getShippingMethods()
     }, [])
 
-    if (!isLogin) {
-        customerService.setShouldBackToCheckout()
-        window.location.replace('/login')
-
-        return null
-    }
-
     return (
-        <div style={{ margin: "auto", maxWidth: "1200px", textAlign: "center" }}>
-            <h1>地址</h1>
-            <p>{data.last_name}{data.first_name}</p>
-            <p>{data.email}</p>
-            <p>{shipping.postcode}{shipping.country}{shipping.state}{shipping.city}{shipping.address_1}</p>
-            <p>{data.payment_method_title}</p>
-            <p>{data.shipping_lines[0].method_title}</p>
-            <Button outlined onClick={
-                (e) => {
-                    setSubmitting(true)
-                    const submitOrder = async () => {
-                        const order = await orderService.submitOrder(data)
-                        if (order) {
-                            cartService.clearCartItems()
-                            window.location.replace(`/orders/${order.id}/success`)
-                        } else {
-                            window.location.replace(`/orders/failed`)
+        <CheckoutInfoContext.Provider value={[submitData, setSubmitData, isReady]} >
+            <ExtraCheckoutInfroContext.Provider value={[extraSubmitData, SetExtraSubmitData]}>
+                <div style={{ margin: "auto", maxWidth: "1200px" }}>
+                    <h1 style={{ textAlign: "center", padding: "18px 0" }}>結帳</h1>
+                    <CheckoutInfoEditorContainer />
+                    <div style={{ textAlign: "center", padding: "18px 0 88px 0" }}>
+                        <Button outlined onClick={
+                            (e) => {
+                                setSubmitting(true)
+                                const submitOrder = async () => {
+                                    console.log(submitData)
+                                    // const order = await orderService.submitOrder(submitData)
+                                    // if (order) {
+                                        // cartService.clearCartItems()
+                                        // window.location.replace(`/orders/${order.id}/success`)
+                                    // } else {
+                                        // window.location.replace(`/orders/failed`)
+                                    // }
+                                }
+                                submitOrder()
+                            }
                         }
-                    }
-                    submitOrder()
-                }
-            }
-                disabled={submitting}
-            >{buttonText}</Button>
-        </div>
+                            disabled={!isReady.current || submitting}
+                        >{getButtonText()}</Button>
+                    </div>
+                </div>
+            </ExtraCheckoutInfroContext.Provider>
+        </CheckoutInfoContext.Provider>
     )
 }
 
 export default CheckoutPage;
-
